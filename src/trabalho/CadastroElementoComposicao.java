4fa2b0a03c9177060c967eabd284657355e4efc2
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2021-04-28 00:26:40 -0300

    CadastroElementoComposicao

--- src/trabalho/CadastroElementoComposicao.java
+++ src/trabalho/CadastroElementoComposicao.java
@@ -1,5 +1,38 @@
 package trabalho;
 
-public class CadastroElementoComposicao {
+import java.util.ArrayList;
 
+public class CadastroElementoComposicao {
+    private ArrayList<ElementoComposicao> elementosDeComposicao;
+
+    public CadastroElementoComposicao () {
+        this.elementosDeComposicao = new ArrayList<>();
+    }
+
+    public void cadastra (ElementoComposicao e) {
+        elementosDeComposicao.add(e);
+    }
+
+    public int quantidade () {
+        return elementosDeComposicao.size();
+    }
+
+    public ElementoComposicao getPorPosicao (int index) {
+        if (index < 0) {
+            return null;
+        } else if (index >= quantidade()) {
+            return null;
+        } else {
+            return elementosDeComposicao.get(index);
+        }
+    }
+
+    public ElementoComposicao getPorId (int id) {
+        for (ElementoComposicao e : elementosDeComposicao) {
+            if (e.getId() == id) {
+                return e;
+            }
+        }
+        return null;
+    }
 }
