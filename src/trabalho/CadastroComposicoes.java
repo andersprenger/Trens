b01b7034ae73ffb43070b04660eb3f6ae1d84243
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2021-04-27 23:00:42 -0300

    CadastroComposicoes

--- src/trabalho/CadastroComposicoes.java
+++ src/trabalho/CadastroComposicoes.java
@@ -1,4 +1,38 @@
 package trabalho;
 
+import java.util.ArrayList;
+
 public class CadastroComposicoes {
+    private ArrayList<Composicao> composicoes;
+
+    public CadastroComposicoes() {
+        composicoes = new ArrayList<>();
+    }
+
+    public void cadastra (Composicao c) {
+        composicoes.add(c);
+    }
+
+    public int quantidade () {
+        return composicoes.size();
+    }
+
+    public Composicao getPorPosicao (int index) {
+        if (index < 0) {
+            return null;
+        } else if (index >= quantidade()) {
+            return null;
+        } else {
+            return composicoes.get(index);
+        }
+    }
+
+    public Composicao getPorId (int id) {
+        for (Composicao c : composicoes) {
+            if (c.getId() == id) {
+                return c;
+            }
+        }
+        return null;
+    }
 }
