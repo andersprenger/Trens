80dc764b69b912a7127caa314ae4c29c807f038c
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2021-04-27 21:21:46 -0300

    Composicao

--- src/trabalho/Composicao.java
+++ src/trabalho/Composicao.java
@@ -1,8 +1,125 @@
 package trabalho;
 
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 public class Composicao {
     private int id;
     private ArrayList<ElementoComposicao> composicao;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getQtdadeLocomotivas() {
+        int count = 0;
+        for (ElementoComposicao e : composicao) {
+            if (e instanceof Locomotiva) {
+                count++;
+            } else {
+                break;
+            }
+        }
+        return count;
+    }
+
+    public int getQtdadeVagoes() {
+        int count = 0;
+        for (ElementoComposicao e : composicao) {
+            if (e instanceof Vagao) {
+                count++;
+            } else {
+                break;
+            }
+        }
+        return count;
+    }
+
+    private ElementoComposicao getElementoComposicao (int posicao) {
+        if (posicao >= 0 && posicao < composicao.size()) {
+            return composicao.get(posicao);
+        }
+        return null;
+    }
+
+    public Locomotiva getLocomotiva (int posicao) {
+        ElementoComposicao e = getElementoComposicao(posicao);
+        if (e == null) {
+            return null;
+        }
+        return e instanceof Locomotiva ? (Locomotiva) e : null;
+    }
+
+    public Vagao getVagao (int posicao) {
+        ElementoComposicao e = getElementoComposicao(posicao);
+        if (e == null) {
+            return null;
+        }
+        return e instanceof Vagao ? (Vagao) e : null;
+    }
+
+    private int maximoVagoes () {
+        int maximoVagoes = 0;
+        for (ElementoComposicao e : composicao) {
+            if (e instanceof Locomotiva) {
+                maximoVagoes += ((Locomotiva) e).getQtdadeMaxVagoes();
+            } else {
+                break;
+            }
+        }
+        return maximoVagoes;
+    }
+
+    private double pesoMaximo () {
+        double pesoMaximo = 0;
+        for (ElementoComposicao e : composicao) {
+            if (e instanceof Locomotiva) {
+                pesoMaximo += ((Locomotiva) e).getPesoMaximo();
+            } else {
+                break;
+            }
+        }
+        return pesoMaximo;
+    }
+
+    private double pesoAtual () {
+        double pesoAtual = 0;
+        for (ElementoComposicao e : composicao) {
+            if (e instanceof Vagao) {
+                pesoAtual += ((Vagao) e).getCapacidadeCarga();
+            }
+        }
+        return pesoAtual;
+    }
+
+    private boolean engataElemento(ElementoComposicao e) {
+        if (e.getIdComposicao() != -1 && e.getIdComposicao() != this.getId()) {
+            return false;
+        } else {
+            e.setIdComposicao(this);
+            composicao.add(e);
+            return true;
+        }
+    }
+
+    public boolean engataLocomotiva(Locomotiva l) {
+        if (getQtdadeVagoes() > 0) {
+            return false;
+        } else {
+            return engataElemento(l);
+        }
+    }
+
+    public boolean engataVagao (Vagao v) {
+        if (getQtdadeLocomotivas() == 0) {
+            return false;
+        } else if (getQtdadeVagoes() == maximoVagoes()) {
+            return false;
+        } else if (pesoAtual() + v.getCapacidadeCarga() > pesoMaximo()) {
+            return false;
+        } else {
+            return engataElemento(v);
+        }
+    }
 }
